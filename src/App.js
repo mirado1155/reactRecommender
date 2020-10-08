8c1ba98bb15ad8c3edead4d9d8c0420217f16669
Robert Adams <rjadams1991@gmail.com>
2020-10-08 12:18:28 -0500

    setting up react project...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react'
 class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-
-        }
-    }
 
     render () {
         return null;
---
+++ src/components/DisplayScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+class DisplayScreen extends React.Component {
+    
+}
